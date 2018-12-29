aebc116cedf214674de7a649c24eff9b9a658128
Vojta Drabek <vojta.drabek@gmail.com>
2018-12-29 17:38:42 +0100

    Logging window appearance and performance

--- caster/lib/log_window.py
+++ caster/lib/log_window.py
@@ -13,7 +13,7 @@ import struct
 
 class TextWindow(object):
 
-    DELAY = 50
+    DELAY = 0.1
     TIMEOUT = 50000
 
     def read_log_record(self, client):
@@ -26,10 +26,10 @@ class TextWindow(object):
         if chunk == b"close":
             self.exit()
         elif chunk == b"hide":
-            self.stext.winfo_toplevel().wm_attributes('-alpha', 0)
+            self.stext.winfo_toplevel().wm_attributes('-alpha', 0.0)
             return None
         elif chunk == b"show":
-            self.stext.winfo_toplevel().wm_attributes('-alpha', 1)
+            self.stext.winfo_toplevel().wm_attributes('-alpha', 1.0)
             return None
         else:
             while len(chunk) < slen:
@@ -38,54 +38,52 @@ class TextWindow(object):
             return logging.makeLogRecord(obj)  
 
     def update_clock(self):
-        #print(".")
-        if self.connect_time > self.TIMEOUT:
-            for client in self.clients:
-                client.close()
-            self.clients = []
-            return
-        try:
-            (clientsocket, address) = self.sock.accept()
-            if clientsocket:
-                self.clients.append(clientsocket)
-                print('Client connected from %s' % str(address))
-        except socket.error as e:
-                if e[0] == 10035:
-                    sys.stdout.write('.')
-                else:
-                    raise
+            while True:
+                if self.connect_time > self.TIMEOUT:
+                    for client in self.clients:
+                        client.close()
+                    self.clients = []
+                    return
+                try:
+                    (clientsocket, address) = self.sock.accept()
+                    if clientsocket:
+                        self.clients.append(clientsocket)
+                        print('Client connected from %s' % str(address))
+                except socket.error as e:
+                        if e[0] == 10035:
+                            sys.stdout.write('.')
+                        else:
+                            raise
 
-        if self.clients:
-            ready_to_read, ready_to_write, in_error = select.select(
-                    self.clients,
-                    [],
-                    [],
-                    1)
-            if ready_to_read:
-                retries = 10
-                for client in ready_to_read:
-                    while retries > 0:
-                        try:
-                            data = self.read_log_record(client)
-                            print(str(data))
-                            if data:
-                                self.display(data)
-                                self.connect_time = 0
-                        except socket.error as e:
-                            if e[0] == 10035:
-                                retries -= 1
-                                time.sleep(self.DELAY//1000)
-                                continue
-                            print(str(e))
-                            client.close()
-                            self.clients.remove(client)
-                            break
-
-        self.stext.after(self.DELAY, threading.Thread(target=self.update_clock).start)
+                if self.clients:
+                    ready_to_read, ready_to_write, in_error = select.select(
+                            self.clients,
+                            [],
+                            [],
+                            1)
+                    if ready_to_read:
+                        retries = 10
+                        for client in ready_to_read:
+                            while retries > 0:
+                                try:
+                                    data = self.read_log_record(client)
+                                    if data:
+                                        self.display(data)
+                                        self.connect_time = 0
+                                except socket.error as e:
+                                    if e[0] == 10035:
+                                        retries -= 1
+                                        time.sleep(self.DELAY)
+                                        continue
+                                    print('Exception: %s' % str(e))
+                                    client.close()
+                                    self.clients.remove(client)
+                                    break
+                time.sleep(self.DELAY)
 
     def display(self, record):
         msg = self.formatter.format(record) + '\n'
-        print(msg)
+        print('Received: %s' % msg)
         self.stext.configure(state='normal')
         self.stext.insert(END, msg, record.levelname)
         self.stext.configure(state='disabled')
@@ -99,7 +97,9 @@ class TextWindow(object):
         self.sock.setblocking(False)
         self.sock.bind(('localhost', logging.handlers.DEFAULT_TCP_LOGGING_PORT))
         self.sock.listen(1)
-        self.stext = ScrolledText.ScrolledText(bg='white', height=20, state="disabled")
+        self.stext = ScrolledText.ScrolledText(bg='white', height=20, width=100, state="disabled")
+        self.stext.winfo_toplevel().title('Caster logging')
+        self.stext.winfo_toplevel().wm_attributes('-topmost')
         self.stext.pack(fill=BOTH, side=LEFT, expand=True)
         self.stext.tag_config('INFO', foreground='black')
         self.stext.tag_config('DEBUG', foreground='gray')
@@ -107,18 +107,18 @@ class TextWindow(object):
         self.stext.tag_config('ERROR', foreground='red')
         self.stext.tag_config('CRITICAL', foreground='red', underline=1)
         self.stext.focus_set()
-        self.stext.after(self.DELAY, self.update_clock)
         self.connect_time = 0
-        print('Listening on test')
-        self.stext.mainloop()    
+        threading.Thread(target=self.update_clock).start()
+        print('Listening on %d' % logging.handlers.DEFAULT_TCP_LOGGING_PORT)
+        self.stext.mainloop()
 
     def exit(self):
         self.sock.close()
         self.stext.quit()
 
 def run():
-    print('run')
-    TextWindow('%(asctime)-15s %(name)-15s %(levelname)-8s %(message)s')
+    print('Running log window')
+    TextWindow('%(asctime)-15s %(name)-10s %(levelname)-8s %(message)s')
 
 if __name__ == "__main__":
     run()
